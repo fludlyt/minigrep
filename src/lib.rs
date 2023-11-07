7387ed621854436a1bbbd1334a5ad16316b8d6cd
Nana Akwah <fludlyt@users.noreply.github.com>
2023-11-07 16:42:42 +0000

    Use env::args iterator directly

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-	pub fn build(args: &[String]) -> Result<Config, &'static str> {
-		if args.len() < 3 {
-			return Err("not enough arguments");
-		}
+	pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+		args.next(); // Ignores the first item which is the path of the executable
 
-		let query = args[1].clone();
-		let file_path = args[2].clone();
+		let query = match args.next() {
+			Some(arg) => arg,
+			None => return Err("Didn't get a query string"),
+		};
+
+		let file_path = match args.next() {
+			Some(arg) => arg,
+			None => return Err("Didn't get a query string"),
+		};
 
 		let ignore_case = env::var("IGNORE_CASE").is_ok();
 
